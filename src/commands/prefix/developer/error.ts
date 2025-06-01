cc126930bdcc5f770e6ba84ca06c8bce6f5e03a3
ThatBadName <thatbadname@outlook.com>
2025-06-01 10:47:31 +0100

    moved error to correct file

--- src/commands/prefix/developer/error.ts
+++ src/commands/prefix/developer/error.ts
@@ -1,8 +1,8 @@
-import { CommandPermission } from "../../constants/permissions";
-import { ErrorSchema } from "../../database/modals/Error";
-import { PrefixCommand } from "../../types/Command";
-import { getCachedDataElse } from "../../utils/database/getCachedElse";
-import { toTimeUnit } from "../../utils/formatters/toTimeUnit";
+import { CommandPermission } from "../../../constants/permissions";
+import { ErrorSchema } from "../../../database/modals/Error";
+import { PrefixCommand } from "../../../types/Command";
+import { getCachedDataElse } from "../../../utils/database/getCachedElse";
+import { toTimeUnit } from "../../../utils/formatters/toTimeUnit";
 
 const cmd: PrefixCommand<{
   error_id: string;
